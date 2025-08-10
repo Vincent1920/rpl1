ddbc233918b62b161d9b38a35d28134e6e8a0675
Vincent1920 <99301380+Vincent1920@users.noreply.github.com>
2025-08-11 00:53:51 +0700

    Update pembayaran.php

--- pembayaran/pembayaran.php
+++ pembayaran/pembayaran.php
@@ -6,9 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/feather-icons"></script>
-    <link rel="stylesheet" href="../css/keranjang.css">
-    <link rel="stylesheet" href="../css/styles.css">
-    <link rel="stylesheet" href="../css/pembayaran.css">
+  
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/feather-icons"></script>
     <!-- SweetAlert2 -->
