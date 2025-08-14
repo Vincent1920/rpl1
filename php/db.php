3efdbf8c30caea4e414dda6db656a7bd4021cdf4
rafli024 <muhammadrafli2460@gmail.com>
2025-08-14 12:06:14 +0700

    change wrong value for variable dbname

--- php/db.php
+++ php/db.php
@@ -12,7 +12,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $host = $_ENV['DB_HOST'];
 $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASS'];
-$dbname = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
